2ca408f794d94b076b6ae74d5ee593c1c8ff5e4d
Obaidul1545 <mdobaidulislam128@gmail.com>
2023-10-20 21:02:35 +0600

    responsive add

--- js/scritp.js
+++ js/scritp.js
@@ -5,4 +5,32 @@ $('.main_slider').slick({
     arrows: true,
     prevArrow: '<i class="fa-solid fa-angle-left prev"></i>',
     nextArrow: '<i class="fa-solid fa-angle-right next"></i>',
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 575.99,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
 });
